0285843bdc70a229e61bd59d1213f91ffb51ba72
brandon <brandon.ngongolo@student.moringaschool.com>
2023-03-24 15:12:09 -0400

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
+  post '/login', to: 'session#create'
+  post '/signup', to:'user#create'
+  get '/me', to: 'user#show'
+  delete '/logout', to: 'session#destroy'
+
 end
